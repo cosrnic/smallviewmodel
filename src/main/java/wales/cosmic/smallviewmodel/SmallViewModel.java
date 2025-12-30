3b9b2c8ae3c07d90187ebdb09434acf9afb1f65c
cosrnic <75631523+cosrnic@users.noreply.github.com>
2025-12-30 19:52:30 +0000

    feat: 1.21.11, /smallviewmodel config command

--- src/main/java/wales/cosmic/smallviewmodel/SmallViewModel.java
+++ src/main/java/wales/cosmic/smallviewmodel/SmallViewModel.java
@@ -3,6 +3,9 @@ package wales.cosmic.smallviewmodel;
 import com.moulberry.lattice.Lattice;
 import com.moulberry.lattice.element.LatticeElements;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import wales.cosmic.smallviewmodel.config.SmallViewModelConfig;
@@ -17,6 +20,16 @@ public class SmallViewModel implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         SmallViewModelConfig.get();
+
+        ClientCommandRegistrationCallback.EVENT.register((dispatcher, ctx) -> {
+            var command = ClientCommandManager.literal("smallviewmodel").executes(context -> {
+                Minecraft.getInstance().schedule(() -> Minecraft.getInstance().setScreen(
+                    getConfigScreen(Minecraft.getInstance().screen, SmallViewModelConfig.get())
+                ));
+                return 0;
+            });
+            dispatcher.register(command);
+        });
     }
 
     public static Screen getConfigScreen(Screen parent, SmallViewModelConfig config) {
